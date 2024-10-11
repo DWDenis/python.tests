a8d398df78181241db7a410b0641707f77e46748
DWDenis <deniswillianpereira02@gmail.com>
2024-10-10 23:01:19 -0300

    commit#3

--- story.py
+++ story.py
@@ -1,3 +1,12 @@
-a = "I\n love\n  apples"
+def first_meeting():
+    print("Hey 👋, you new here, ain't ya?")
+    print("Sooo 👉👈, where are you from? 😊")
+    global place
+    place = input()
+    print("Yooo that's crazy, never heard of it. 😐")
 
-print(a)
+def know_each_oder():
+    print("Since you kinda new here, let me guide you.")
+    print("I'm Fairy \"THE MIGHTY RAVEN\" 🌩️🐦‍⬛🌩️")
+
+first_meeting()

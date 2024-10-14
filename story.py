28b615a11290072e06fb71ce62c2a3746062d071
DWDenis <deniswillianpereira02@gmail.com>
2024-10-14 12:00:43 -0300

    commit#6

--- story.py
+++ story.py
@@ -7,6 +7,19 @@ def first_meeting():
 
 def know_each_oder():
     print("Since you kinda new here, let me guide you.")
-    print("I'm Fairy \"THE MIGHTY RAVEN\" 🌩️🐦‍⬛🌩️")
+    print("By the way, I'm Fairy \"THE MIGHTY RAVEN\" 🌩️🐦‍⬛🌩️")
+    print("It is quite my pleasure to be acquaintance with you")
+    print("So what is your name?")
+    global name
+    name = input()
+    know_name = print(f"Good name \"{name}\", it does rolls well in the tongue, does it have any meaning? ")
+    answer = input("Yes/No?")
+    if answer.lower == "Yes":
+        print("Cool, cool")
+    else:
+        print("There's no way, come on go look if you find something comeback!")
+
+
 
 first_meeting()
+know_each_oder()

40274903164f153479af9a14e5cf960ebfadea67
DWDenis <deniswillianpereira02@gmail.com>
2024-12-01 10:18:43 -0300

    NemSeiMais

---
+++ Desafio_Mapa.c
@@ -0,0 +1,44 @@
+/*
+===============DADOS BASICOS=====================================
+
+Dias de operacao [1 - Quinta, 2 - Sexta, 3 - Sabado, 4 - Domingo]
+
+Dados Clientes:
+    Nome;
+    CPF;
+    Quantidade de pessoas
+=================================================================
+*/
+
+/*
+========== FAZER RESERVA ========================================
+Coletar Nome, CPF, Dia da reserva e quatidade de pessoas
+
+=================================================================
+
+
+========== LISTAR RESERVA ======================================
+Listar as reservas cadastradas no formato:
+Nome: Xxxxxxx Xxxxxxx Xxxxxxx (str)
+CPF: xxx.xxx.xxx-xx (int)
+Dia: X Xxxxxx (str)
+Numero de Pessoas: X (int)
+
+=================================================================
+
+
+========= TOTAL DE PESSOAS POR DIA =============================
+Visualizar a quantidade de total de pessoas em um determinado dia
+
+=================================================================
+
+
+========= SAIR =================================================
+Encerrar programa
+
+=================================================================
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>

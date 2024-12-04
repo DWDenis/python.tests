9041345d280590f3b9944a2989ff659f1b95a77e
DWDenis <deniswillianpereira02@gmail.com>
2024-12-04 09:36:58 -0300

    commit#banco

--- Desafio_Mapa.c
+++ Desafio_Mapa.c
@@ -1,3 +1,7 @@
+
+#include <stdio.h>
+#include <string.h>
+
 /*
 ===============DADOS BASICOS=====================================
 
@@ -9,14 +13,49 @@ Dados Clientes:
     Quantidade de pessoas
 =================================================================
 */
+typedef struct {
+    char nome[100];
+    char cpf[15];
+    int dia;  // 1 - Quinta, 2 - Sexta, 3 - Sábado, 4 - Domingo
+    int numPessoas;
+} Reserva;
+
+Reserva reservas[100];  // Array de reservas, suportando até 100 reservas
+int totalReservas = 0;  // Contador de reservas
+
 
 /*
 ========== FAZER RESERVA ========================================
 Coletar Nome, CPF, Dia da reserva e quatidade de pessoas
 
 =================================================================
+*/
+void fazerReserva() {
+    if (totalReservas < 100) {
+        printf("Digite o nome do cliente: ");
+        fgets(reservas[totalReservas].nome, sizeof(reservas[totalReservas].nome), stdin);
+        reservas[totalReservas].nome[strcspn(reservas[totalReservas].nome, "\n")] = 0;  // Remove a quebra de linha
+
+        printf("Digite o CPF do cliente: ");
+        fgets(reservas[totalReservas].cpf, sizeof(reservas[totalReservas].cpf), stdin);
+        reservas[totalReservas].cpf[strcspn(reservas[totalReservas].cpf, "\n")] = 0;  // Remove a quebra de linha
+
+        printf("Digite o dia da reserva (1 - Quinta, 2 - Sexta, 3 - Sábado, 4 - Domingo): ");
+        scanf("%d", &reservas[totalReservas].dia);
+
+        printf("Digite o número de pessoas: ");
+        scanf("%d", &reservas[totalReservas].numPessoas);
+        getchar();  // Limpar o buffer do teclado
+
+        totalReservas++;
+        printf("Reserva feita com sucesso!\n");
+    } else {
+        printf("Não é possível fazer mais reservas. Capacidade máxima atingida.\n");
+    }
+}
 
 
+/*
 ========== LISTAR RESERVA ======================================
 Listar as reservas cadastradas no formato:
 Nome: Xxxxxxx Xxxxxxx Xxxxxxx (str)
@@ -25,20 +64,95 @@ Dia: X Xxxxxx (str)
 Numero de Pessoas: X (int)
 
 =================================================================
+*/
+void listarReservas() {
+    if (totalReservas == 0) {
+        printf("Nenhuma reserva encontrada.\n");
+    } else {
+        for (int i = 0; i < totalReservas; i++) {
+            char *diaReserva;
+            switch (reservas[i].dia) {
+                case 1: diaReserva = "Quinta"; break;
+                case 2: diaReserva = "Sexta"; break;
+                case 3: diaReserva = "Sábado"; break;
+                case 4: diaReserva = "Domingo"; break;
+                default: diaReserva = "Dia inválido"; break;
+            }
+
+            printf("\nReserva %d:\n", i + 1);
+            printf("Nome: %s\n", reservas[i].nome);
+            printf("CPF: %s\n", reservas[i].cpf);
+            printf("Dia: %s\n", diaReserva);
+            printf("Número de Pessoas: %d\n", reservas[i].numPessoas);
+        }
+    }
+}
 
 
+/*
 ========= TOTAL DE PESSOAS POR DIA =============================
 Visualizar a quantidade de total de pessoas em um determinado dia
 
 =================================================================
-
-
-========= SAIR =================================================
-Encerrar programa
-
-=================================================================
 */
+void totalPessoasPorDia() {
+    int diaConsulta;
+    printf("Digite o dia para consultar o total de pessoas (1 - Quinta, 2 - Sexta, 3 - Sábado, 4 - Domingo): ");
+    scanf("%d", &diaConsulta);
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+    int totalPessoas = 0;
+    for (int i = 0; i < totalReservas; i++) {
+        if (reservas[i].dia == diaConsulta) {
+            totalPessoas += reservas[i].numPessoas;
+        }
+    }
+
+    char *diaConsultaStr;
+    switch (diaConsulta) {
+        case 1: diaConsultaStr = "Quinta"; break;
+        case 2: diaConsultaStr = "Sexta"; break;
+        case 3: diaConsultaStr = "Sábado"; break;
+        case 4: diaConsultaStr = "Domingo"; break;
+        default: diaConsultaStr = "Dia inválido"; break;
+    }
+
+    printf("Total de pessoas no dia %s: %d\n", diaConsultaStr, totalPessoas);
+}
+
+
+// Exibe MENU de opcoes
+void exibirMenu() {
+    int opcao;
+    do {
+        printf("\n=== MENU ===\n");
+        printf("1. Fazer Reserva\n");
+        printf("2. Listar Reservas\n");
+        printf("3. Total de Pessoas por Dia\n");
+        printf("4. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        getchar();  // Limpar o buffer do teclado
+
+        switch (opcao) {
+            case 1:
+                fazerReserva();
+                break;
+            case 2:
+                listarReservas();
+                break;
+            case 3:
+                totalPessoasPorDia();
+                break;
+            case 4:
+                printf("Finalizando registro\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 4);
+}
+
+int main() {
+    exibirMenu();
+    return 0;
+}
--- outros/Banco_otimizado.py
+++ outros/Banco_otimizado.py
@@ -1,63 +1,157 @@
-menu = '''
-[d]  Deposito
-[s]  Saque
-[e]  Extrato
-[q] Sair
+def menu():
+    menu = """\n
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
 
-=> '''
 
-d_valor= 0
-s_valor = 0
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\nOperação falhou! O valor informado é inválido.")
 
-#precisa ser positivo
-def Deposito():
-    global d_valor 
-    d_valor = int(input("Qual o valor que deseja depositar: "))
-    if d_valor < 2:
-        print("O valor de deposito precisa ser maior que 2")
-    return print(f"Um deposito foi realizado no valor de R$ {d_valor}")
+    return saldo, extrato
 
-# max de 500
-# limite 3 por dia
-def Saque():
-    global s_valor, d_valor
-    print(f"O valor disponivel e de: {d_valor}")
-    
-    i = 0
-    while i <= 2:
-        s_valor = int(input("Qual o valor do saque: "))
-        
-        if s_valor > 500:
-            print("Limite de saque diaria de [R$ 500.00]")
-        elif s_valor > d_valor:
-            print(f"Valor disponivel {d_valor}")
-        else:
-            d_valor -= s_valor
-            print(f"O saldo atual e de: R$ {d_valor}")
-        
-        outro_saque = input("Deseja realizar mais um saque? s/n =>  ")
-        if outro_saque == 'n':
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("\nOperação falhou! Você não tem saldo suficiente.")
+
+    elif excedeu_limite:
+        print("\nOperação falhou! O valor do saque excede o limite.")
+
+    elif excedeu_saques:
+        print("\nOperação falhou! Número máximo de saques excedido.")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
+
+    else:
+        print("\nOperação falhou! O valor informado é inválido.")
+
+    return saldo, extrato
+
+
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+    print("==========================================")
+
+
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\nJá existe usuário com esse CPF!")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\nUsuário não encontrado, fluxo de criação de conta encerrado!")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
+
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
+
+            saldo, extrato = depositar(saldo, valor, extrato)
+
+        elif opcao == "s":
+            valor = float(input("Informe o valor do saque: "))
+
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor=valor,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            )
+
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+
+        elif opcao == "lc":
+            listar_contas(contas)
+
+        elif opcao == "q":
             break
-        
-        i += 1
 
-# Deve retornar o valor atual apos operacoes
-# valor deve ser exibido no formato R$ com "." para separacao de decimais
-def Extrato():
-    global d_valor, s_valor
-    extrato_atual = d_valor - s_valor
-    print(f"O extrato atual e de: R$ {extrato_atual:.2f}")
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
 
 
-while True:
-    resposta = input("Ola, por gentileza escolher umas das opcoes abaixo: " + menu) 
-
-    if resposta == "d":
-        Deposito()
-    elif resposta == "s":
-        Saque()
-    elif resposta == "e":
-        Extrato()
-    elif resposta == "q":
-        print("Tenha um bom dia!")
-        break
+main()
